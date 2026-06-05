c8ac49791197920e8053925b39d6eb134fc56335
Tomyka <tomyka@gmail.com>
2026-06-05 23:19:54 +0300

    feat: add Sign in with Google button to login modal

--- resources/views/modals/login.blade.php
+++ resources/views/modals/login.blade.php
@@ -17,4 +17,14 @@
                                 </div>
                     </div>
                 </form>
+
+                <div class="text-center px-3 pb-3">
+                    <hr class="my-2">
+                    <a href="{{ route('auth.google') }}" class="btn btn-outline-secondary w-100">
+                        <img src="https://www.svgrepo.com/show/475656/google-color.svg"
+                             width="18" height="18" class="me-2" alt="Google">
+                        Prisijungti su Google
+                    </a>
+                </div>
+
             </div>
