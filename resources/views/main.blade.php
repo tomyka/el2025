e5861dd4643084095bd7eaacfb215ac9d230f613
Tomyka <tomyka@gmail.com>
2026-06-05 22:42:27 +0300

    feat: clean up main.blade.php, wrap sections in sb-card

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,74 +1,58 @@
 @extends('layouts.master')
 @section('content')
 
-    <div class="container-fluid">
+<div class="container-fluid px-0">
+    @auth
+        @if(session('disabled') == '')
+            <div class="sb-card">
+                @include('partials.rules')
+            </div>
+        @else
+            <div class="sb-card">
+                @include('partials.fee')
+                @include('partials.messages')
+                @include('partials.warnings')
+            </div>
 
-        @auth
-
-            @if(session('disabled')=="")
-                <div class="row">
-
-                </div>
-                <BR>
-                <div class="row">
-                    <div class="col-md-12 col-12">
-                        <B>Taisyklės:</B>
-                    </div>
-                    @include('partials.rules')
-                </div>
-            @else
-                <div class="card border-primary">
-                    <div class="col col-12 col-md-12">
-                        @include('partials.fee')
-                    </div>
-                    <div class="col col-12 col-md-12">
-                        @include('partials.messages')
-                    </div>
-                    <div class="col col-md-12">
-                        @include('partials.warnings')
-                    </div>
-                </div>
-                <BR>
-                <div class="row">
-                    @if (session('eventRate')==2)
-                        <div class="col col-xl-5 col-lg-6 col-md-12 col-l-12">
-                    @else
-                        <div class="col col-xl-6 col-lg-6 col-md-12 col-l-12">
-                    @endif
-                    @if (session('eventID')!=0)
+            <div class="row g-3">
+                @if(session('eventRate') == 2)
+                    <div class="col-xl-5 col-lg-6 col-12">
+                @else
+                    <div class="col-xl-6 col-lg-6 col-12">
+                @endif
+                    @if(session('eventID') != 0)
                         @include('partials.games')
                         @include('partials.previous')
                         @include('partials.standings')
                     @else
                         @include('partials.points')
                     @endif
+                </div>
 
-                    </div>
-
-                    @if (session('eventRate')==2)
-                        <div class="col col-xl-4 col-lg-6 col-md-12 col-l-12">
+                @if(session('eventRate') == 2)
+                    <div class="col-xl-4 col-lg-6 col-12">
+                @else
+                    <div class="col-xl-6 col-lg-6 col-12">
+                @endif
+                    @if(session('eventID') != 0)
+                        @include('partials.points')
                     @else
-                        <div class="col col-xl-6 col-lg-6 col-md-12 col-l-12">
+                        @include('partials.pointsStandings')
                     @endif
+                </div>
 
-                    @if (session('eventID')!=0)
-                            @include('partials.points')
-                    @else
-                            @include('partials.pointsStandings')
-                   @endif
+                @if(session('eventRate') == 2)
+                    <div class="col-xl-3 col-lg-6 col-12">
+                        @include('partials.pointsStandings')
                     </div>
-
-                    @if (session('eventRate')==2)
-                        <div class="col col-xl-3 col-lg-6 col-md-12 col-l-12">
-                            @include('partials.pointsStandings')
-                        </div>
-                    @endif
-
-
-            @endif
-        @else
+                @endif
+            </div>
+        @endif
+    @else
+        <div class="sb-card">
             @include('welcome')
-        @endauth
-
+        </div>
+    @endauth
 </div>
+
 @endsection
